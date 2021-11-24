278d792df5b31a69d448787b9b26688785a9c6f5
Donald Brower <donald.brower1@gmail.com>
2021-11-23 22:18:50 -0800

    api code for compare uses the sacramento api base

--- _src/js/compare/api.js
+++ _src/js/compare/api.js
@@ -1,59 +1,62 @@
-import axios from 'axios';
-import {descending} from 'd3-array';
+import axios from "axios";
+import { descending } from "d3-array";
 
+// const API_BASE = 'https://live-open-budget-oakland-administration.pantheonsite.io' +
+//   '/wp-json/obo/v1';
 
-const API_BASE = 'https://live-open-budget-oakland-administration.pantheonsite.io' +
-  '/wp-json/obo/v1';
+const API_BASE = "http://198.199.114.93:8000/" + "?rest_route=/obo/v1";
 // const API_BASE = '/data/compare';
 
 const typePaths = {
-  spending: '/fiscal-years-expenses',
-  revenue: '/fiscal-years-revenue',
+  spending: "/fiscal-years-expenses",
+  revenue: "/fiscal-years-revenue",
 };
 
 const dimensionPaths = {
-  department: '/depts',
-  category: '/account-cats',
+  department: "/depts",
+  category: "/account-cats",
 };
 
 const dimensionKeys = {
-  department: 'department',
-  category: 'account_category',
+  department: "department",
+  category: "account_category",
 };
 
-export function fetchBreakdownData (years, yearTypes, type, dimension) {
+export function fetchBreakdownData(years, yearTypes, type, dimension) {
   // start two concurrent requests, one per year;
   // wait for them both to return before ending the fetch
   const urls = years.map((year) => {
     // return API_BASE + typePaths[type] + dimensionPaths[dimension] + `/${year}.json`;
-    return API_BASE + typePaths[type] + dimensionPaths[dimension] + '/' + year;
+    return API_BASE + typePaths[type] + dimensionPaths[dimension] + "/" + year;
   });
-  return axios.all(urls.map(url => axios.get(url)))
-    .then(axios.spread((...budgets) => {
+  return axios.all(urls.map((url) => axios.get(url))).then(
+    axios.spread((...budgets) => {
       // put the data in the thing
       // TODO: filter by budget type, API returns records from all
-      return budgets.map((b,i) => b.data.reduce((acc, row) => {
-        // filter rows that don't match the desired budget type;
-        // double-equals because it might be an integer in string form
-        if (row.budget_type == yearTypes[i]) {
-          // convert to object and cast totals to numbers
-          acc[row[dimensionKeys[dimension]]] = +row.total;
-        }
-        return acc;
-      }, {}));
-    }));
+      return budgets.map((b, i) =>
+        b.data.reduce((acc, row) => {
+          // filter rows that don't match the desired budget type;
+          // double-equals because it might be an integer in string form
+          if (row.budget_type == yearTypes[i]) {
+            // convert to object and cast totals to numbers
+            acc[row[dimensionKeys[dimension]]] = +row.total;
+          }
+          return acc;
+        }, {})
+      );
+    })
+  );
 }
 
-export function fetchTotals () {
+export function fetchTotals() {
   // return axios.get(API_BASE + typePaths.spending + '/totals.json')
-  return axios.get(API_BASE + typePaths.spending)
-  .then(response => {
+  return axios.get(API_BASE + typePaths.spending).then((response) => {
     const data = response.data;
-    data.sort((a,b) => {
-      // sort in reverse chronological order, 
+    data.sort((a, b) => {
+      // sort in reverse chronological order,
       // then adjusted,adopted,proposed within each year
-      const [indexA, indexB] = [a,b].map(record => {
-        const year = record.fiscal_year_range.slice(2,4);
+      const [indexA, indexB] = [a, b].map((record) => {
+        const year = record.fiscal_year_range.slice(2, 4);
         // type numbers don't really correspond to the order we want;
         // this rearranges them
         const type = 6 / record.budget_type;
